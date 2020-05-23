34945bdd7534faaf4068d238fc288d5827181144
julia2003-pixel <juliaregina03@outlook.com>
2020-05-22 23:50:35 -0300

    quarentena

---
+++ exquarentena.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Exercício Quarentena</title>
+    </head>
+    <body>
+        <?php
+            if(empty($_POST)){
+                echo'<form action="exequarentena.php" method="POST">
+                    <input type="text" name="n1" placeholder="Digite um valor..."/> 
+                    +
+                    <input type="text" name="n2" placeholder="Digite um valor..."/> 
+                    =	
+		            <input type="text" name="resp" value=" " placeholder="Resposta..."/>
+				
+		            <input type="submit" value="Somar" />
+		            <input type="reset" value="Apagar" />
+                </form>';
+            }
+            else{
+                $n1=$_POST["n1"];
+                $n2=$_POST["n2"];
+                $resp=$n1+$n2;
+
+                echo '<form action="exquarentena.php" method="POST">
+                    <input type="text" name="n1" value="'.$n1.'" placeholder="Digite um valor..."/> +
+                    <input type="text" name="n2" value="'.$n2.'" placeholder="Digite um valor..."/> =	
+                    <input type="text" name="resp" value="'.$resp.'"placeholder="Resposta..."/>
+                        
+                    <input type="submit" value="Somar" />
+                    <input type="reset" value="Apagar" />
+                </form>';
+                }
+        ?>
+    </body>
+</html>
