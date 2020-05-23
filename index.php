073c535ef56cd7d7ed7a22bf633a4b74d8fc03f1
julia2003-pixel <juliaregina03@outlook.com>
2020-05-23 00:41:08 -0300

    versão2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Exercício Quarentena</title>
+    </head>
+    <body>
+        <?php
+		<p>
+			<a href "exquarentena.php" > exquarentena </a>
+		</p>
+	</body>
+</html>
